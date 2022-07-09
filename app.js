2fa5f52674daa5e98649a1b121504215d3cef617
Nadine101 <nadinet026@gmail.com>
2022-07-09 14:47:25 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let company = {
+    name: 'masson_shose',
+    yearly_revenue: 3000000,
+    ceo: 'joy',
+    Number_of_employees: 100
+}
+if (company['yearly_revenue'] >= 1000000) {
+    console.log("congradulations to: ${company['name']},to the Ceo ${company['ceo']} and their $company['number_of_employees]}employee");
+} else {
+    console.log('more');
+}
+
+let manager =  {
+    name: ['Jason','mark']
+    ['manager' = 0 ]
+    }
